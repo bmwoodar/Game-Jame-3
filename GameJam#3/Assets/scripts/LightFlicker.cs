856ef5e8cbec310c9dfd7418fbd0b4b26d1cb37c
git-stop-please <tshauger2126@gmail.com>
2019-10-24 16:09:43 -0700

    Tylers First Commit

---
+++ GameJam#3/Assets/scripts/LightFlicker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    Light testLight;
+    public float minWaitTimeOff;
+    public float maxWaitTimeOff;
+    public float minWaitTimeOn;
+    public float maxWaitTimeOn;
+    // Use this for initialization
+    void Start () {
+        testLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+	}
+
+    IEnumerator Flashing()
+    {
+        while(true)
+        {
+ /*
+            while (true)
+            {
+                yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+                testLight.enabled = !testLight.enabled;
+            }
+*/
+
+            if (testLight == testLight.enabled)
+            {
+                yield return new WaitForSeconds(Random.Range(minWaitTimeOff, maxWaitTimeOff));
+                testLight.enabled = !testLight.enabled;
+            }
+            if (testLight == !testLight.enabled)
+            {
+                yield return new WaitForSeconds(Random.Range(minWaitTimeOn, maxWaitTimeOn));
+                testLight.enabled = !testLight.enabled;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
