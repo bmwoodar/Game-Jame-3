25b3a717115f06a085717731c800cff7ca72f364
gander16 <56103224+gander16@users.noreply.github.com>
2019-10-24 20:48:46 -0700

    Player UI and Scene Management
    Added player UI and scene management scripts.

---
+++ GameJam#3/Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start() {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            StartCoroutine(WaitForTime());
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
